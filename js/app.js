de3a859a8f080ecd0ecb1d2dcd73e08ba8bdb8a2
EDWARD REGALADO <TREGALADO30@GMAIL.COM>
2021-01-13 23:04:03 -0800

    update bio and css files

--- js/app.js
+++ js/app.js
@@ -19,46 +19,51 @@ console.log('Hello World');
 //question 1
 var answerOne = prompt('Do I live in Seattle?').toLowerCase();
 if (answerOne === 'yes' || answerOne === 'y') {
-  console.log('You\'re correct!');
-  alert('You\'re correct');
+  //console.log('You\'re correct!');
+  alert('You\'re correct!');
 } else if (answerOne === 'no' || answerOne === 'n') {
-  console.log('You\'re WRONG!');
+  //console.log('You\'re WRONG!');
   alert('You\'re WRONG!');
 }
 
 //question 2
 var answerTwo = prompt('Do I own a dog?').toLocaleLowerCase();
 if (answerTwo === 'yes' || answerTwo === 'y') {
-  console.log('you are correct');
-  alert('you are correct');
+  ///console.log('you are correct');
+  alert('Yes, her name is Lemon');
 } else if (answerTwo === 'no' || answerTwo === 'n') {
-  console.log('That\'s not right!');
-  alert('That\'s not right!');
+  //console.log('That\'s not right...);
+  alert('That\'s not right...');
 }
 
 //question 3
 var answerThree = prompt('Do I like to Snowboard?').toLocaleLowerCase();
 if (answerThree === 'yes' || answerThree === 'y') {
+  //console.log('I love to Snowboard!');
   alert('I love to Snowboard!');
 } else if (answerThree === 'no' || answerThree === 'n') {
-  console.log('Did you even read my Bio?');
-  alert('Did you even read my Bio?');
+  //console.log('Did you even read my Bio? \:)');
+  alert('Did you even read my Bio? \:)');
 }
 
 //question 4
 var answerFour = prompt('Was I born in Chicago?').toLocaleLowerCase();
 if (answerFour === 'yes' || answerFour === 'y') {
+  //console.log('Spot on!');
   alert('Spot on!');
 } else if (answerFour === 'no' || answerFour === 'n') {
-  console.log('You don\'t know me at all!');
+  //console.log('You don\'t know me at all!');
   alert('You don\'t know me at all!');
 }
 
 //question 5
-var answerFive = prompt('Was I in my Military?').toLocaleLowerCase();
+var answerFive = prompt('Did I serve in the Military?').toLocaleLowerCase();
 if (answerFive === 'yes' || answerFive === 'y') {
-  console.log('Yes, I served my country for almost 8 years');
-  alert('Yes, I served my country for almost 8 years');
+  //console.log('Yes, I served my country for almost 8 years');
+  alert('I served my country for almost 8 years');
+} else if (answerFive === 'no' || answerFive === 'n') {
+  console.log('I feel like you don\'t know me at all :(');
+  alert('I really feel like you don\'t know me at all :(');
 }
 
 
