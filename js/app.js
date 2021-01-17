3764c7bbe6bfbabcdd652993f307c3b49289be13
EDWARD REGALADO <TREGALADO30@GMAIL.COM>
2021-01-16 19:39:34 -0800

    updated for and while questions

--- js/app.js
+++ js/app.js
@@ -16,12 +16,14 @@ console.log('Hello World');
 //Greeting message
 var userName = prompt('Hello, what\'s your name?');
 alert('Hello ' + userName + ', nice to meet you!');
+var pointTotal = 0;
 
 //question 1
 var answerOne = prompt('Do I live in Seattle?').toLowerCase();
 if (answerOne === 'yes' || answerOne === 'y') {
   //console.log('You\'re correct!');
   alert('You\'re correct!');
+  pointTotal++;
 } else if (answerOne === 'no' || answerOne === 'n') {
   //console.log('You\'re WRONG!');
   alert('You\'re WRONG!');
@@ -32,16 +34,19 @@ var answerTwo = prompt('Do I own a dog?').toLocaleLowerCase();
 if (answerTwo === 'yes' || answerTwo === 'y') {
   ///console.log('you are correct');
   alert('Yes, her name is Lemon');
+  pointTotal++;
 } else if (answerTwo === 'no' || answerTwo === 'n') {
   //console.log('That\'s not right...);
   alert('That\'s not right...');
 }
+
 
 //question 3
 var answerThree = prompt('Do I like to Snowboard?').toLocaleLowerCase();
 if (answerThree === 'yes' || answerThree === 'y') {
   //console.log('I love to Snowboard!');
   alert('I love to Snowboard!');
+  pointTotal++;
 } else if (answerThree === 'no' || answerThree === 'n') {
   //console.log('Did you even read my Bio? \:)');
   alert('Did you even read my Bio? :)');
@@ -52,23 +57,79 @@ var answerFour = prompt('Was I born in Chicago?').toLocaleLowerCase();
 if (answerFour === 'yes' || answerFour === 'y') {
   //console.log('Spot on!');
   alert('Spot on!');
+  pointTotal++;
 } else if (answerFour === 'no' || answerFour === 'n') {
   //console.log('You don\'t know me at all!');
   alert('You don\'t know me at all!');
 }
+
 
 //question 5
 var answerFive = prompt('Did I serve in the Military?').toLocaleLowerCase();
 if (answerFive === 'yes' || answerFive === 'y') {
   //console.log('Yes, I served my country for almost 8 years');
   alert('I served my country for almost 8 years');
+  pointTotal++;
 } else if (answerFive === 'no' || answerFive === 'n') {
   //console.log('I feel like you don\'t know me at all :(');
   alert('I really feel like you don\'t know me at all :(');
 }
 
-//Goodbye message
-alert('Thanks for visiting my web page, ' + userName + '!');
+
+
+//question 6
+
+var guess = 0;
+var myAge = 38;
+var correctGuess = false;
+while (guess < 4 && !correctGuess) {
+  guess++;
+  var guessAge = parseInt(prompt('How old do you think I am?'));
+  //console.log(typeof guessAge, guessAge);
+  if (guessAge < myAge) {
+    alert('sorry, that\'s too young.');
+  } else if (guessAge > myAge) {
+    alert('Aw, that is too old.');
+  } else if (guessAge === 38) {
+    alert(`Correct ${userName} ! `);
+    correctGuess = true;
+    pointTotal++;
+  }
+
+  if (!correctGuess) {
+    alert(`You're out of chances! The correct answer was ${myAge}`);
+  }
+
+//question 79
+  var whatFood = ['pizza', 'pasta', 'mexican', 'sushi', 'peanut butter', 'kimchi'];
+  var guessFood = prompt('Can you guess what my favorite foods are?').toLocaleLowerCase();
+  var answeredCorrect = false;
+  var attemptsRemaining = 6;
+  while (attemptsRemaining > 0 && !answeredCorrect) {
+    attemptsRemaining--;
+    for (var i = 0; i < whatFood.length; i++) {
+      if (guessFood === whatFood[i]) {
+        answeredCorrect = true;
+        //console.log(`I love to eat ${whatFood}`);
+        alert(`I love to eat ${whatFood}`);
+        pointTotal++;
+      }
+    }
+    if (attemptsRemaining > 0 && !answeredCorrect) {
+      guessFood = prompt('I don\'t like that food. Please try again!').toLocaleLowerCase();
+    }
+    if (attemptsRemaining === 0 && !answeredCorrect) {
+      console.log( `Tough luck ${userName}! My favorite foods are pizza, pasta, mexican, sushi, peanut butter and kimchi`);
+      alert(`Tough luck ${userName}! My favorite foods are pizza, pasta, mexican, sushi, penaut butter and kimchi`);
+    }
+  }
+}
+
+
+//Good bye message
+alert(`Your total points is ${pointTotal}`);
+alert('Thank for visiting my web page, ' + userName + '!');
+
 
 
 
